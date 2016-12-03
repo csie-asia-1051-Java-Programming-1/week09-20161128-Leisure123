d841cd076a3bdde407e2709fa27da8fbe73c1145
Leisure123 <105021011@live.asia.edu.tw>
2016-12-03 19:15:33 +0800

    Week09(未完成

--- src/ex/ex01.java
+++ src/ex/ex01.java
@@ -1,8 +1,4 @@
 ﻿package ex;
-
-import java.util.Random;
-import java.util.Scanner;
-
 /*
  * Topic: 請設計一樂透亂數選號程式，由 1~42 中選出 6個不重覆的數字組合並輸出。 請使用已下方式,(setSeed就是設定亂數種子，可以使每次跑出的亂數序列，都會是一樣的)
  例如:輸入 23323456會得到 39  17  32  13  41  15 
@@ -11,19 +7,32 @@ import java.util.Scanner;
  * Date: 2016/11/28
  * Author: 105021011 丁紹剛
  */
-import java.util.Scanner;
 import java.util.Random;
+import java.util.Scanner;
 public class ex01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scn = new Scanner(System.in);
-		Random randnum = new Random();
-		randnum.setSeed(scn.nextInt());
+		Random rnd = new Random();
+		int data[] = new int [6];
+		int n = 0;
+		boolean check;
 		for(int i = 0 ; i < 6 ; i++){
-			System.out.print(randnum.nextInt(42)+1+" ");
+			check = true;
+			while(check){
+				check = false;
+				data[i] = rnd.nextInt(42)+1;
+				for(int j = 0 ; j < i ; j++){
+					if(data[j] == data[i]){
+						check = true;
+					}
+				}
+			}
 		}
-
+		for(int i = 0 ; i < 6 ; i++){
+		System.out.print(data[i] + " ");
+		}
 	}
 
 }
--- src/hw/hw02.java
+++ src/hw/hw02.java
@@ -4,12 +4,26 @@ package hw;
 數列一:1 2 4 7 11 16 22 29…
 數列二:1 2 2 3 3 3 4 4 4 4 5 5 5 5 5...
  * Date: 2016/11/28
- * Author: 1050210XX 周永振老師
+ * Author: 105021011 丁紹剛
  */
+import java.util.Scanner;
 public class hw02 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		int sum = 1;
+		for(int i = 1 ; sum <= n ; i++){
+			System.out.print(sum+ " ");
+			sum = sum + i;
+		}
+		System.out.print("\n");
+		for(int i = 1 ; i <= n ; i++){
+			for(int j = i ; j > 0 ; j--){
+				System.out.print(i + " ");
+			}
+		}
 
 	}
 
